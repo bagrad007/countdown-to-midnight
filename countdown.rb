9c4c04f8aef605751cf06bdc9e6fecf45e2e150c
Luke Gill <bagrad007@gmail.com>
2021-01-12 15:02:38 -0500

    commit tree fedcd7356020697a36ed7343c7439dd640978028

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+    while n > 0
+        puts "#{n} SECOND(S)!"
+        n -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+countdown(5)
+
+def countdown_with_sleep(n)
+    while n > 0
+        puts "#{n} SECONDS(S)!"
+        n -= 1
+       sleep 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+countdown_with_sleep(5)
